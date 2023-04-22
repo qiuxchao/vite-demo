ff8d91abb3300d0e8683345cb6a2bd3854a53021
qiuxchao <2915241235@qq.com>
2023-04-22 23:26:20 +0800

    🦄 refactor: code restore

---
+++ 02-vite&vue3/src/pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+	<div style="border: 1px solid aquamarine; padding: 10px;" class="text-center">
+		<h1 class="text-[26px] font-medium text-red-500">
+			About Page
+			<br />Wow, this is About page!
+		</h1>
+	</div>
+</template>
